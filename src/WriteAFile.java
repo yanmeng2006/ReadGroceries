785af5cbdfc0ba272905df095b1d37d50ea53ead
yanmeng2006 <3138821687@qq.com>
2025-03-21 17:14:51 +0800

    commit

--- src/WriteAFile.java
+++ src/WriteAFile.java
@@ -1,23 +1,31 @@
 import java.io.FileWriter;
+import java.util.Scanner;
 import java.io.BufferedWriter;
 
 public class WriteAFile {
+    Scanner scanner = new Scanner(System.in);
     String writtenFile = "C:\\Users\\Meng\\Desktop\\Programming2\\WriteAFileExample.txt.txt";
     FileWriter filewriter;
     BufferedWriter writer;
+    String test;
 
 public void writeFile() {
     try {
         filewriter = new FileWriter(writtenFile);
         writer = new BufferedWriter(filewriter);
-        writer.write("Hello, World!");
-        writer.newLine();
+        System.out.println("Enter the text you want to write to the file here.");
+        while(true) {
+            test = scanner.nextLine();
+            if(test.equalsIgnoreCase("END")){
+                break;
+            }
+            writer.write(test);
+        }
+        System.out.println("File written successfully.");
+        writer.flush();
         writer.close();
     } catch (Exception e) {
         e.printStackTrace();
     }
-}
-public String toString() {
-    return writtenFile;
 }
 }
